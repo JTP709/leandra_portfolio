08a838f351580e312a318f4bd6504663c67dfeaf
Jon <JTP709@gmail.com>
2018-08-10 08:46:09 -0400

    update portfolio test data to match new schema

--- server/resources/dbscripts/testPortfolioData.js
+++ server/resources/dbscripts/testPortfolioData.js
@@ -2,7 +2,7 @@ const testPortfolio = [
   {
     rank: 1,
     title: "Qroger",
-    thumbnail: "http://placekitten.com/g/512/256",
+    thumbnail: "http://placekitten.com/g/512/384",
     carousel: [
       {
         img: "http://placekitten.com/g/1024/512",
@@ -44,7 +44,7 @@ const testPortfolio = [
   {
     rank: 2,
     title: "Kwitter",
-    thumbnail: "https://placedog.net/512/256",
+    thumbnail: "https://placedog.net/512/384",
     carousel: [
       {
         img: "http://placekitten.com/g/1024/512",
@@ -86,7 +86,133 @@ const testPortfolio = [
   {
     rank: 3,
     title: "Kroogle",
-    thumbnail: "http://placekitten.com/g/512/256",
+    thumbnail: "http://placekitten.com/g/512/384",
+    carousel: [
+      {
+        img: "http://placekitten.com/g/1024/512",
+        label: "First slide label",
+        subLabel: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+      },
+      {
+        img: "https://placedog.net/1024/512",
+        label: "Second slide label",
+        subLabel: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      }, 
+      {
+        img: "http://placekitten.com/g/1024/512",
+        label: "Third slide label",
+        subLabel: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+      }
+    ],
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+		"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+		"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+		"nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+		"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
+		"pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+		"culpa qui officia deserunt mollit anim id est laborum.",
+    front_end: [
+      'JabbaScript',
+      'KTML 5',
+      'KSS 3'
+    ],
+    back_end: [
+      'Jabba',
+      'Esspress',
+      'MandoBH'
+    ],
+    hidden: false,
+    github: 'https://www.github.com',
+    demo: 'https://www.google.com'
+  },
+  {
+    rank: 2,
+    title: "Kwitter",
+    thumbnail: "https://placedog.net/512/384",
+    carousel: [
+      {
+        img: "http://placekitten.com/g/1024/512",
+        label: "First slide label",
+        subLabel: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+      },
+      {
+        img: "https://placedog.net/1024/512",
+        label: "Second slide label",
+        subLabel: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      }, 
+      {
+        img: "http://placekitten.com/g/1024/512",
+        label: "Third slide label",
+        subLabel: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+      }
+    ],
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+		"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+		"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+		"nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+		"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
+		"pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+		"culpa qui officia deserunt mollit anim id est laborum.",
+    front_end: [
+      'JabbaScript',
+      'KTML 5',
+      'KSS 3'
+    ],
+    back_end: [
+      'Jabba',
+      'Esspress',
+      'MandoBH'
+    ],
+    hidden: false,
+    github: 'https://www.github.com',
+    demo: 'https://www.google.com'
+  },
+  {
+    rank: 3,
+    title: "Kroogle",
+    thumbnail: "http://placekitten.com/g/512/384",
+    carousel: [
+      {
+        img: "http://placekitten.com/g/1024/512",
+        label: "First slide label",
+        subLabel: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+      },
+      {
+        img: "https://placedog.net/1024/512",
+        label: "Second slide label",
+        subLabel: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      }, 
+      {
+        img: "http://placekitten.com/g/1024/512",
+        label: "Third slide label",
+        subLabel: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+      }
+    ],
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+		"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+		"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+		"nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+		"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
+		"pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+		"culpa qui officia deserunt mollit anim id est laborum.",
+    front_end: [
+      'JabbaScript',
+      'KTML 5',
+      'KSS 3'
+    ],
+    back_end: [
+      'Jabba',
+      'Esspress',
+      'MandoBH'
+    ],
+    hidden: false,
+    github: 'https://www.github.com',
+    demo: 'https://www.google.com'
+  },
+  {
+    rank: 2,
+    title: "Kwitter",
+    thumbnail: "https://placedog.net/512/384",
     carousel: [
       {
         img: "http://placekitten.com/g/1024/512",
